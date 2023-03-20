544405ea05c635c07feefee2db100f2fb11d6294
Talinee <bieteuy@gmail.com>
2023-03-20 17:16:22 +0700

    first commit

---
+++ basic_type/basic_one.ts
@@ -0,0 +1,21 @@
+let a: string;
+let b: boolean;
+let c: string;
+
+type User = {
+  name: string;
+  surname: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+const user: User = {
+  name: "John",
+  surname: "Smith",
+  age: 30,
+  isAdmin: true,
+};
+
+const mul = (a: number,b: number):number => a + b;
+
+const add = (a: number,b: number):number => a + b;
